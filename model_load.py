c9cee5c1ecfc9b0e682df3687fc1e31052caf2f7
shutouyusei <syutou.yusei.061@s.kyushu-u.ac.jp>
2025-11-20 16:03:39 +0900

    [fix]import path

--- model_load.py
+++ model_load.py
@@ -1,4 +1,11 @@
 import torch
+import sys
+import os
+CURRENT_FILE_PATH = os.path.abspath(__file__)
+PROJECT_ROOT = os.path.dirname(CURRENT_FILE_PATH)
+if PROJECT_ROOT not in sys.path:
+    sys.path.append(PROJECT_ROOT)
+
 from act.act_model import ActModel
 from mlp.mlp_model import MlpModel
 
